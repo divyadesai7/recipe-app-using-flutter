96d5639423c913c34694ba3b591d523aa733a604
Mohammed Misran <mohammed.misran38@gmail.com>
2021-12-05 03:11:32 +0530

    models

---
+++ recipe_app/lib/models/recipe.dart
@@ -0,0 +1,13 @@
+class Recipe {
+  String? title;
+  String? type;
+  String? prepTime;
+  String? image;
+
+  Recipe({
+    this.title,
+    this.type,
+    this.prepTime,
+    this.image,
+  });
+}
