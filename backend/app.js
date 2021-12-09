e3a47e6ab8751dbb86d887da8279520c505dbe39
Mohammed Misran <mohammed.misran38@gmail.com>
2021-12-09 19:22:53 +0530

    refactored app to use mvc

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,20 @@
 const fs = require('fs');
 const express = require('express');
 
+const categoryRouter = require('./routes/categoryRoutes.js');
+
 // Create express app
 const app = express();
 
-// Get categories from file
-app.get('/api/v1/categories', (req, res) => {
-  try {
-    const data = fs.readFileSync('./public/data/categories.json', { encoding: 'utf8' });
-    const categories = JSON.parse(data);
-    res.json({ status: 'success', categories });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ status: 'fail', message: 'Something went wrong.' });
-  }
-});
+// Body-parser
+app.use(express.json({ limit: '10kb' }));
 
-// Get recipes from file
-app.get('/api/v1/recipes', (req, res) => {
-  try {
-    const data = fs.readFileSync('./public/data/recipes.json', { encoding: 'utf8' });
-    const recipes = JSON.parse(data);
-    res.json({ status: 'success', recipes });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ status: 'fail', message: 'Something went wrong.' });
-  }
-});
+// Routers
+app.use('/api/v1/categories', categoryRouter);
 
+// Unhandled routes
 app.all('*', (req, res) => {
   res.status(404).json({ status: 'fail', message: `${req.url} not found.` });
 });
 
-// Create server
-const port = 3000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
