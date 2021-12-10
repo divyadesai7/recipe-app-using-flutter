fd0dbc9dfd8ef4c6c625a529dd26b4284930fd28
Divya Desai <desai721522@gmail.com>
2021-12-10 19:58:17 +0530

    main file

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/screens/home_screen/home_screen.dart';
 // import 'package:recipe_app/screens/home_screen/home_screen.dart';
 // import 'package:recipe_app/screens/login_screen/login_screen.dart';
 import 'package:recipe_app/screens/splash_screen/splash_screen.dart';
