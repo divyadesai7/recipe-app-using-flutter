d2884ef50e99343ae2b3997f5a0fe0e6de49c56f
Divya Desai <desai721522@gmail.com>
2021-12-12 00:31:03 +0530

    added onSaved property

--- recipe_app/lib/components/rounded_input_field.dart
+++ recipe_app/lib/components/rounded_input_field.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/components/text_field_container.dart';
 import 'package:recipe_app/constants.dart';
+import 'package:recipe_app/models/user.dart';
 
 class RoundedInputField extends StatelessWidget {
   final String hintText;
   final IconData icon;
   final bool obscureText;
+  final User user;
 
   const RoundedInputField({
     required this.hintText,
     required this.icon,
     this.obscureText = false,
+    required this.user,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
-      child: TextField(
-        obscureText: obscureText,
-        onChanged: (value) {},
-        cursorColor: primaryLightColor,
-        style: TextStyle(
-          color: primaryLightColor,
-          fontSize: 18,
-          fontWeight: FontWeight.w600,
-        ),
-        decoration: InputDecoration(
-          icon: Icon(icon, color: primaryLightColor),
-          hintText: hintText,
-          hintStyle: TextStyle(color: primaryLightColor),
-          border: InputBorder.none,
-        ),
-      ),
+      child: TextFormField(
+          obscureText: obscureText,
+          cursorColor: primaryLightColor,
+          style: TextStyle(
+            color: primaryLightColor,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+          decoration: InputDecoration(
+            icon: Icon(icon, color: primaryLightColor),
+            hintText: hintText,
+            hintStyle: TextStyle(color: primaryLightColor),
+            border: InputBorder.none,
+          ),
+          onSaved: (value) {
+            if (obscureText) {
+              user.password = value as String;
+              return;
+            }
+            user.email = value as String;
+          }),
     );
   }
 }
