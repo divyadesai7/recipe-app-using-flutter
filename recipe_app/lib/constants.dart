bdba7dfbef9a07b2766530d55c0b6ce47e0f32e0
Divya Desai <desai721522@gmail.com>
2021-12-12 00:29:34 +0530

    added shadow color

--- recipe_app/lib/constants.dart
+++ recipe_app/lib/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color.fromRGBO(255, 133, 24, 1);
+const shadowColor = Color.fromRGBO(255, 133, 24, 0.8);
 const primaryLightColor = Colors.white;
 const primaryDarkColor = Colors.black;
 const bgColor = Color.fromRGBO(232, 232, 234, 1);
